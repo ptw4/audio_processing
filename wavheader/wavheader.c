5547e91d16883b51e985d7c0cb8397cb85ee489a
thorff1 <thorff1@gmail.com>
2015-09-24 10:25:42 -0400

    WAV header parsing utility
    Uploading the WAV header parser I made on the morning of 9/23/15

---
+++ wavheader/wavheader.c
@@ -0,0 +1,151 @@
+/* Created By Philip Washy *
+ * September 2015          *
+ * ptw4@pitt.edu           */
+
+/*TODO:                                                                 *
+ *	>change to using a buffered reader for efficiency                  x*
+ *	>move file pointer and WAVHEADER into the main method for security x*
+ *	>move header stuff into a header file                              x*
+ *	>add redundant corruption/error checking for safety                \*
+ *	>adjust main method so it can be used as a utility in other exes   x*/
+
+#include "wavheader.h"
+
+uint32_t convertTo32 ( char * Data, ENDIANESS Endian ) {
+	if( Endian == little ) {
+		return ( Data[ 3 ] | ( Data[ 2 ] << 8 ) | ( Data[ 1 ] << 16 ) | ( Data[ 0 ] << 24 ) );
+	} else {
+		return ( Data[ 0 ] | ( Data[ 1 ] << 8 ) | ( Data[ 2 ] << 16 ) | ( Data[ 3 ] << 24 ) );
+	}
+}
+
+uint16_t convertTo16 ( char * Data, ENDIANESS Endian ) {
+	if( Endian == little ) {
+		return ( Data[ 1 ] | ( Data[ 0 ] << 8 ) );
+	} else {
+		return ( Data[ 0 ] | ( Data[ 1 ] << 8 ) );
+	}
+}
+
+int parseHeader ( char * FileName, WAVHEADER * Header ) {
+	char * TwoBytes, * FourBytes, * Buffer;
+	ENDIANESS Endian = big; //0 = big, 1 = little
+	TwoBytes = ( char * )( malloc( sizeof( char ) * 2 ) );
+	FourBytes = ( char * )( malloc( sizeof( char ) * 4 ) );
+	FILE * AudioPhile = fopen( FileName, "r+" );
+	//FILE * LogFile = fopen( "log.hex", "w+" );
+
+	int i, Chunk2Size;
+	Buffer = ( char * )( malloc( sizeof( char ) * 18 ) );
+	fgets( Buffer, 19, AudioPhile );
+
+	//fputs( Buffer, LogFile );
+
+	/*RIFF header*/
+	for( i = 0; i < 4; i++ ) {
+		Header->ChunkID[ i ] = Buffer[ i ];
+	}
+	if( !( strcmp( Header->ChunkID, "RIFX" ) ) ) {
+		Endian = little;
+	} else if( strcmp( Header->ChunkID, "RIFF" ) ) {
+		return ( 1 );
+	}
+	for( i = 0; i < 4; i++ ) {
+		FourBytes[ i ] = Buffer[ i + 4 ];
+	}
+	Header->ChunkSize = convertTo32( FourBytes, Endian );
+	for( i = 0; i < 4; i++ ) {
+		Header->Format[ i ] = Buffer[ i + 8 ];
+	}
+	if( strcmp( Header->Format, "WAVE" ) ) {
+		return ( 2 );
+	}
+
+
+	/*Format Subchunk*/
+	for( i = 0; i < 4; i++ ) {
+		Header->SubChunk1ID[ i ] = Buffer[ i + 12 ];
+	}
+	for( i = 0; i < 4; i++ ) {
+		FourBytes[ i ] = Buffer[ i + 16 ];
+	}
+	Header->SubChunk1Size = convertTo32( FourBytes, Endian );
+	if( Header->SubChunk1Size < 16 ) {  //handle corrupted data
+		return ( 3 );
+	}
+
+	free( Buffer ); //Free up the buffer and then grab the next chunk
+	Chunk2Size = Header->SubChunk1Size + 8;
+	Buffer = ( char * )( malloc( sizeof( char ) * Chunk2Size ) );
+	fgets( Buffer, ( Chunk2Size + 1 ), AudioPhile );
+
+	//fputs( Buffer, LogFile );
+
+	for( i = 0; i < 2; i++ ) {
+		TwoBytes[ i ] = Buffer[ i ];
+	}
+	Header->AudioFormat = convertTo16( TwoBytes, Endian );
+	for( i = 0; i < 2; i++ ) {
+		TwoBytes[ i ] = Buffer[ i + 2 ];
+	}
+	Header->NumChannels = convertTo16( TwoBytes, Endian );
+	for( i = 0; i < 4; i++ ) {
+		FourBytes[ i ] = Buffer[ i + 4 ];
+	}
+	Header->SampleRate = convertTo32( FourBytes, Endian );
+	for( i = 0; i < 4; i++ ) {
+		FourBytes[ i ] = Buffer[ i + 8 ];
+	}
+	Header->ByteRate = convertTo32( FourBytes, Endian );
+	for( i = 0; i < 2; i++ ) {
+		TwoBytes[ i ] = Buffer[ i + 12 ];
+	}
+	Header->BlockAlign = convertTo16( TwoBytes, Endian );
+	for( i = 0; i < 2; i++ ) {
+		TwoBytes[ i ] = Buffer[ i + 14 ];
+	}
+	Header->BitsPerSample = convertTo16( TwoBytes, Endian );
+
+
+	/*Data Subchunk, sans data*/
+	for( i = 0; i < 4; i++ ) {
+		Header->SubChunk2ID[ i ] = Buffer[ i + Header->SubChunk1Size ];
+	}
+	for( i = 0; i < 4; i++ ) {
+		FourBytes[ i ] = Buffer[ i + Header->SubChunk1Size + 4 ];
+	}
+	Header->SubChunk2Size = convertTo32( FourBytes, Endian );
+
+	free( Buffer );
+	free( TwoBytes );
+	free( FourBytes );
+	//fclose( LogFile );
+	fclose( AudioPhile );
+	return ( 0 );
+}
+
+int getWAVHeader ( char * FileName, WAVHEADER * Header, int ErrorOut ) {
+/*int main ( int argc, char ** argv ) {   This code for testing purposes 
+	WAVHEADER * Header = ( WAVHEADER * )( malloc( sizeof( WAVHEADER ) ) );
+	char * FileName = "440.wav";
+	int ErrorOut = 1;*/
+	switch( parseHeader( FileName, Header ) ) {
+		case 3:
+			if( ErrorOut ) {
+				fputs( "Corrupted File: Unable to Read", stderr );
+			}
+			return ( 3 );
+		case 2:
+			if( ErrorOut ) {
+				fputs( "Invalid file Type: Not a Wave File", stderr );
+			}
+			return ( 2 );
+		case 1:
+			if( ErrorOut ) {
+				fputs( "Invalid File Type: Not a RIFF or RIFX File", stderr );
+			}
+			return ( 1 );
+		default:
+			return ( 0 );
+	}
+}
---
+++ wavheader/wavheader.h
@@ -0,0 +1,67 @@
+/* Created By Philip Washy *
+ * September 2015          *
+ * ptw4@pitt.edu           */
+
+#ifndef _STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef _STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef _STDINT_H
+#include <stdint.h>
+#endif
+
+#ifndef _STRING_H
+#include <string.h>
+#endif
+
+#ifndef _WAVHEADER_H
+#define _WAVHEADER_H 1
+
+typedef struct _WAVHEADER {
+	/*RIFF header*/
+	char     ChunkID[ 4 ];     //should be "RIFF"
+	uint32_t ChunkSize;
+	char     Format[ 4 ];      //should be "WAVE"
+
+	/*Format subchunk*/
+	char     SubChunk1ID[ 4 ]; //should be "fmt "
+	uint32_t SubChunk1Size;
+	uint16_t AudioFormat;      //LPCM = 1
+	uint16_t NumChannels;
+	uint32_t SampleRate;
+	uint32_t ByteRate;         //= SampleRate * NumChannels * BitsPerSample/8
+	uint16_t BlockAlign;       //= NumChannels * BitsPerSample/8
+	uint16_t BitsPerSample;    //maybe use BitsPerSample for error checking?
+
+	/*Data subchunk*/
+	char     SubChunk2ID[ 4 ]; //should be "data"
+	uint32_t SubChunk2Size;
+} WAVHEADER;
+
+typedef enum _ENDIANESS { little, big } ENDIANESS;
+
+int getWAVHeader ( char * FileName, WAVHEADER * Header, int ErrorOut ); //given a file name, wavheader struct pointer, and the option for stderr output (0 or !0), this fills the wavheader struct
+
+//check to see if this actually works for conversion
+/*
+typedef union {
+	unsigned char c[ 4 ];
+	uint32_t      l;
+} NUM32;
+
+typedef union {
+	unsigned char c[ 2 ];
+	uint16_t      s;
+} NUM16;
+*/
+/* yes this does work, but someone on stackexchange         *
+ * who had suggested something similar as the solution      *
+ * to a problem was informed that this is technically       *
+ * undefined behavior so best just be safe and leave it out */
+//also no handling differences in endianess
+
+#endif
